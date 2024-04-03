4270f57f620d5713468ec65e909ed3d452069c5c
max-montes <77820353+max-montes@users.noreply.github.com>
2024-04-03 19:35:41 -0400

    bottom 3 attributes

--- Bottom3Attributes.py
+++ Bottom3Attributes.py
@@ -1,5 +1,16 @@
 import yfinance as yf
 
+# Get ticket
 msft = yf.Ticker("MSFT")
 
-print(msft.info)
+# Get data
+hist = msft.history(period="max")
+curr = msft.info
+
+# Get components for calculation of Larry Williams Percent Range
+highest_high = hist['High'].max()
+lowest_low = hist['Low'].min()
+current_close = curr['regularMarketPreviousClose']
+
+# Calculate Larry Williams Percent Range
+williams_percent_range = ((highest_high - current_close) / (highest_high - lowest_low)) * -100
