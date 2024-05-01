eeacb67ee1077dd13cf83bff1888fbc72980448e
rasorro <rasorro@bc.edu>
2024-04-30 21:46:11 -0400

    Fix Hyperparameter Plots
    Fixed issue in previous hyperparameter plots that caused them to not properly model the 2D datums being supplied

--- BoostedDecisionStumps.py
+++ BoostedDecisionStumps.py
@@ -70,22 +70,32 @@ def model_ticker(ticker, period=182):
     # Extracting results from grid search
     results = grid_search.cv_results_
     params = results['params']
-    mean_test_scores = results['mean_test_score']
+    scores = results['mean_test_score'].reshape(len(hyper_grid['learning_rate']), len(hyper_grid['n_estimators']))
 
     # Plotting hyperparameter tuning graphs
     fig, ax = plt.subplots(1, 2, figsize=(12, 5))
 
     # Plotting n_estimators vs. mean test score
-    ax[0].plot([param['n_estimators'] for param in params], mean_test_scores, marker='o')
+    for i, lr in enumerate(hyper_grid['learning_rate']):
+        ax[0].plot(hyper_grid['n_estimators'], scores[i, :], label=f"Learning Rate: {lr}", marker='o', linestyle=':')
+    ax[0].plot(hyper_grid['n_estimators'], scores.mean(0), label=f"Average Score", lw=3)
     ax[0].set_xlabel('Number of Estimators')
     ax[0].set_ylabel('Mean Test Score (Precision)')
     ax[0].set_title('Hyperparameter Tuning: Number of Estimators')
+    ax[0].set_xticks(hyper_grid['n_estimators'])
+    ax[0].legend()
+    ax[0].grid(True)
 
     # Plotting learning_rate vs. mean test score
-    ax[1].plot([param['learning_rate'] for param in params], mean_test_scores, marker='o')
+    for i, n_est in enumerate(hyper_grid['n_estimators']):
+        ax[1].plot(hyper_grid['learning_rate'], scores[:, i], label=f"# of Estimators: {n_est}", marker='o', linestyle=':')
+    ax[1].plot(hyper_grid['learning_rate'], scores.mean(1), label=f"Average Score", lw=3)
     ax[1].set_xlabel('Learning Rate')
     ax[1].set_ylabel('Mean Test Score (Precision)')
     ax[1].set_title('Hyperparameter Tuning: Learning Rate')
+    ax[1].set_xticks(hyper_grid['learning_rate'])
+    ax[1].legend()
+    ax[1].grid(True)
 
     plt.tight_layout()
     plt.show()
